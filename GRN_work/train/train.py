26305abd7b08e20f5d6f2ecfb7e7578d70eceb6a
DuncanBrady <robert.duncan.brady@gmail.com>
2025-07-09 12:33:09 +1000

    added additional flags for loss and output options, added try catch block to handle incorrect flag usage and updated error message along with usage information to be more understandable

--- GRN_work/train/train.py
+++ GRN_work/train/train.py
@@ -1,16 +1,40 @@
 # This is the training script that will be used to train multiple autoencoders.
 import getopt, sys
 
+usage = "Usage: python train.py -f <input_directory> -o <output_directory> (-m <model> | -e <encoder> -d <decoder>) [-l <loss_function>]"
 
-arglist = sys.argv[1:]
-opts, args = getopt.getopt(arglist, "h", ["help"])
-if len(opts) == 0 or ("-h", "") in opts or ("--help", "") in opts:
-    print("Usage: python train.py [options]")
-    print("Options:")
-    print("  -h, --help      Show this help message and exit")
-    print("-m, --model     Specify the model to train (e.g., 'autoencoder', 'variational_autoencoder')")
-    print("-d, --dataset   Specify the dataset to use for training (e.g., 'mnist', 'cifar10')")
-    print("-l, --loss      Specify the loss function to use (e.g., 'mse', 'bce')")
-    print("-e, --encoder   Specify the encoder architecture (e.g., 'simple', 'resnet')")
-    print("-d, --decoder   Specify the decoder architecture (e.g., 'simple', 'resnet')")
-    sys.exit(0)
+try:
+    arglist = sys.argv[1:]
+    opts, args = getopt.getopt(arglist, "hm:f:o:l:e:d:", ["help", "model=", "file=", "output=", "loss=", "encoder=", "decoder="])
+
+    if len(opts) == 0 or ("-h", "") in opts or ("--help", "") in opts:
+        print(usage)
+        print("Required Args:")
+        print("  -f, --file      Specify the dataset file to use for training (e.g., 'mnist', 'cifar10')")
+        print("  -o, --output    Specify the output directory for saving the trained model, logs, and results")
+        print("\tModel Specification:(choose one of the following)")
+        print("\t -m, --model     Specify the model to train (e.g., 'autoencoder', 'variational_autoencoder'), cannot be used with --encoder or --decoder")
+        print("\t\tOR:")
+        print("\t -e, --encoder   Specify the encoder architecture (e.g., 'simple', 'resnet'), must be used with --decoder")
+        print("\t -d, --decoder   Specify the decoder architecture (e.g., 'simple', 'resnet'), must be used with --encoder")
+        print("Optional Args:")
+        print("  -l, --loss      Specify the loss function to use (e.g., 'mse', 'bce')")
+        sys.exit(0)
+    if ("-m", "") in opts or ("--model", "") in opts:
+        if ("-e", "") in opts or ("--encoder", "") in opts or ("-d", "") in opts or ("--decoder", "") in opts:
+            print("Error: Cannot use --model with --encoder or --decoder. Please choose one.")
+            sys.exit(1)
+        print("Choosing model to train...")
+        exit(0)
+    elif ("-e", "") in opts or ("--encoder", "") in opts and ("-d", "") in opts or ("--decoder", "") in opts:
+        print("Choosing encoder-decoder architecture...")
+        exit(0)
+    else:
+        print("Error: Incorrect arguments provided. Please use -h or --help for usage instructions.")
+        print(usage)
+        print("Exiting...")
+        sys.exit(1)
+except getopt.GetoptError as err:
+    print(f"Error: {err}")
+    print(usage)
+    sys.exit(2)
