4eaf6d59db4092ac98cfa7af91a17f036482c671
DuncanBrady <robert.duncan.brady@gmail.com>
2025-07-24 11:31:45 +1000

    Added tests for case of encoder - decoder use, loss function, learning rate and epochs

--- GRN_work/src/train/config.py
+++ GRN_work/src/train/config.py
@@ -1,10 +1,11 @@
 import yaml
 
 with open('C:\\Users\\rdbra\\Documents\\honoursProject\\code_base\\Hon_proj_RDB\\GRN_work\\src\\train\\config.yaml', "r") as file:
-    config = yaml.safe_load(file)
+    config = yaml.load(file, Loader=yaml.Loader)
 
 data_path = config.get("data_path", "data/")
 models = config.get("models")
 encoders = config.get("encoders", {})
 decoders = config.get("decoders", {})
+encoder_decoder_pairs = config.get("encoder_decoder_pairs", {})
 
