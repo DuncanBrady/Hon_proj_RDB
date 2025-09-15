514b7aee5f2fe55ce2b78a61f1944bebfeb2738e
DuncanBrady <robert.duncan.brady@gmail.com>
2025-09-16 01:18:47 +1000

    added dir for pbs jobs and created 4 jobs

---
+++ GRN_work/PBSJobs/fullData_VQ.sh
@@ -0,0 +1,18 @@
+#! bin/bash
+
+#PBS -P yr31
+#PBS -q gpuvolta
+#PBS -l walltime=8:00:00
+#PBS -l ncpus=24
+#PBS -l mem=32GB
+#PBS -l gpus=2
+#PBS -l jobfs=100GB
+#PBS -l wd
+
+module load python/3.9.2
+module load pytorch/1.10.0
+source  /home/561/rb6232/code_base/Hon_proj_RDB/.venv/bin/activate
+python3 train_vqvae2_binned.py --data-file /g/data/yr31/rb6232/sct_matrix_transposed.npz --out-dir /g/data/yr31/rb6232/fullData_VQ --num-bins 7
+
+
+
---
+++ GRN_work/PBSJobs/fullData_fc.sh
@@ -0,0 +1,17 @@
+#! bin/bash
+
+#PBS -P yr31
+#PBS -q gpuvolta
+#PBS -l walltime=8:00:00
+#PBS -l ncpus=24
+#PBS -l mem=32GB
+#PBS -l gpus=2
+#PBS -l jobfs=100GB
+#PBS -l wd
+
+module load python/3.9.2
+module load pytorch/1.10.0
+source  /home/561/rb6232/code_base/Hon_proj_RDB/.venv/bin/activate
+python3 train_simple_autoencoder.py --data_path /g/data/yr31/rb6232/sct_matrix_transposed.npz --num_bins 7 --track_epoch_confusion
+
+
