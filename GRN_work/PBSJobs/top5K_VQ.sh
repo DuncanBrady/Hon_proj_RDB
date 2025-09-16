a791c89cd0133ad1a1dec11e4f5e06ab1192d252
DuncanBrady <robert.duncan.brady@gmail.com>
2025-09-16 10:51:27 +1000

    changed out-dir to home due to access issues on gadi

--- GRN_work/PBSJobs/top5K_VQ.sh
+++ GRN_work/PBSJobs/top5K_VQ.sh
@@ -12,7 +12,7 @@
 module load python3/3.9.2
 module load pytorch/1.10.0
 source  /home/561/rb6232/code_base/Hon_proj_RDB/.venv/bin/activate
-python3 /home/561/rb6232/code_base/Hon_proj_RDB/GRN_work/train_vqvae2_binned.py --data-file /home/561/rb6232/code_base/data/sct_top5k.npz --out-dir /g/data/yr31/rb6232/top5k_VQ --num-bins 7
+python3 /home/561/rb6232/code_base/Hon_proj_RDB/GRN_work/train_vqvae2_binned.py --data-file /home/561/rb6232/code_base/data/sct_top5k.npz --out-dir /home/561/rb6232/code_base/results/top5k_VQ --num-bins 7
 
 
 
