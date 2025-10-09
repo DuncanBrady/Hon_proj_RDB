8c9bf7dfca9afc4b3c49bae4aa65c89373fd1745
DuncanBrady <robert.duncan.brady@gmail.com>
2025-10-09 13:20:53 +1100

    added rough code for UMAP, pca and tsne in dim_reduce.py

--- GRN_work/analysis/dataVisualisation/dim_reduce.py
+++ GRN_work/analysis/dataVisualisation/dim_reduce.py
@@ -1,15 +1,19 @@
 # file containing functions for dimensionality reduction and visualisation of high-dimensional data
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import umap 
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
 
 def pca_reduction(data, n_components=2):
-    from sklearn.decomposition import PCA
+  
     pca = PCA(n_components=n_components)
     reduced_data = pca.fit_transform(data)
     return reduced_data, pca.explained_variance_ratio_
 
 def tsne_reduction(data, n_components=2, perplexity=30, n_iter=1000):
-    from sklearn.manifold import TSNE
     tsne = TSNE(n_components=n_components, perplexity=perplexity, n_iter=n_iter)
     reduced_data = tsne.fit_transform(data)
     return reduced_data
